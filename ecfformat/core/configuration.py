daba2dda2d86aad4fc49df3dc298e44d1eda8d9c
Roger Marsh <roger.marsh@solentware.co.uk>
2022-08-22 15:18:02 +0000

    Correction to setup.cfg file.

--- ecfformat/core/configuration.py
+++ ecfformat/core/configuration.py
@@ -8,7 +8,6 @@ The initial values are taken from file named in self._CONFIGURATION in the
 user's home directory if the file exists.
 
 """
-# This gets a pylint no-name-in-module report but the code runs.
 from solentware_misc.core import configuration
 
 from . import constants
